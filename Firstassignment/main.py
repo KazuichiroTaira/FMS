a7e547651e917151d7646713c2aad2f408d16e25
kazuichirotaira <ktaria098@gmail.com>
2020-09-19 14:46:12 +0300

    Second commit

--- Firstassignment/main.py
+++ Firstassignment/main.py
@@ -8,7 +8,21 @@ FIG_FOLDER = 'fig'
 
 os.makedirs(FIG_FOLDER, exist_ok=True)
 
-data_file = os.path.join(DATA_FOLDER, 'akay_001-200900.csv')
-df = pd.read_csv(data_file)
+data_file = os.path.join(DATA_FOLDER, 'data.csv')
+df = pd.read_csv(data_file, sep=";", index_col=[0])
 
+print(df.head())
+# print(df.index)
 
+print(df.columns)
+
+print(df.loc['01-26  Total'].astype("float") )
+
+# c_list = ['Participated, % Women 2009', 'Time used hours,minutes Total 1979']
+
+for c_name in df.columns:
+    try:
+        df[c_name] = df[c_name].astype('float')
+    except ValueError:
+        print(f'got troubles with {c_name}')
+# print(df["Time used hours,minutes Total 1979"])
