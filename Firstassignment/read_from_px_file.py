558f52a9c25d73b31db31da92b68d9864476f69d
AurelienNioche <nioche.aurelien@gmail.com>
2020-09-19 17:39:39 +0300

    add pyx parser

--- Firstassignment/read_from_px_file.py
+++ Firstassignment/read_from_px_file.py
@@ -1,6 +1,6 @@
-import px_reader
+from px_reader import px_reader
 
-px_obj = px_reader.Px("akay_001_200900.px")
+px_obj = px_reader.Px("data/akay_001_200900.px")
 
 df = px_obj.pd_dataframe()
-print(df)
+print(df.columns)
